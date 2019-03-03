1248f2fa67d7759e940390840346f8a2c98ba780
amanda <ajshack4@gmail.com>
2019-03-02 18:47:31 -0800

    moved photoViewer to GraphicsView.py and cleaned up code

--- GraphicsView.py
+++ GraphicsView.py
@@ -2,6 +2,13 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt, QPoint, pyqtSignal, QRect
 from PyQt5.QtWidgets import QMainWindow, QApplication, QGraphicsScene, QGraphicsView, QRubberBand
 from PyQt5.QtGui import QPixmap, QPainter, QPen
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from main import *
+from GraphicsView import *
+import sys
+import time
 
 class GraphicsView(QGraphicsView):
     rectChanged = pyqtSignal(QRect)
@@ -30,3 +37,52 @@ class GraphicsView(QGraphicsView):
     def mouseReleaseEvent(self, event):
         self.changeRubberBand = False
         QGraphicsView.mouseReleaseEvent(self, event)
+
+class photoViewer(object):
+    def __init__(self, ogImage, ogImageScene, width, height):
+        self.ogImage = ogImage
+        self.ogImageScene = ogImageScene
+        self._zoom = 0
+        self._width = width
+        self._height = height
+
+
+    def setDefaultImage(self, image):
+        self.image = image
+        pixmap = self.scale(self._width, self._height)
+        self.updatePixmap(pixmap)
+        return(pixmap)
+
+    def updatePixmap(self, pixmap):
+        self.ogImageScene.clear()
+        self.ogImageScene.addPixmap(pixmap)
+        self.ogImageScene.setSceneRect(QtCore.QRectF(0.0, 0.0, pixmap.width(), pixmap.height()))
+
+    def scale(self, width, height):
+        if (self.image.isNull()):
+            return(QPixmap())
+        return(self.image.scaled(width, height, QtCore.Qt.KeepAspectRatio))
+
+    def zoom(self, pixmap, factor):
+        pixmap = self.scale(pixmap.width()*factor, pixmap.height()*factor)
+        self.updatePixmap(pixmap)
+        return(pixmap)
+        
+    def zoomIn(self, pixmap):
+        self._zoom += 1
+        factor = 1.25
+        return(self.zoom(pixmap, factor))
+    
+    def zoomOut(self, pixmap):
+        if self._zoom == 0:
+            return(pixmap)
+        self._zoom -= 1
+        factor = 0.75
+        return(self.zoom(pixmap, factor))
+    
+    def zeroZoom(self, pixmap):
+        if self._zoom == 0:
+            return(pixmap)
+        pixmap = self.scale(self._width, self._height) #### self.pixmap is used for scaling and anything else
+        self.updatePixmap(pixmap)
+        return (pixmap)
--- MyApp.py
+++ MyApp.py
@@ -1,34 +1,54 @@
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
-from main import Ui_MainWindow
+from main import *
+from GraphicsView import *
 import sys
 import time
 
+# class Action(object):
+#     def __init__(self):
+
+
+#     def paintEvent(self, event):
+#         painter = QtGui.QPainter(self.ogImage)
+#         painter.drawPixmap(self.ogImage.rect(), self.image)
+
+#     def mousePressEvent(self, event):
+#         if event.button() == Qt.LeftButton:
+#             self.drawing = True
+#             self.lastPoint = event.pos()
+
+#     def mouseMoveEvent(self, event):
+#         if event.buttons() and Qt.LeftButton and self.drawing:
+#             painter = QPainter(self.image)
+#             painter.setPen(QPen(Qt.red, 3, Qt.SolidLine))
+#             painter.drawLine(self.lastPoint, event.pos())
+#             self.lastPoint = event.pos()
+#             self.ogImage.update()
+
+#     def mouseReleaseEvent(self, event):
+#         if event.button == Qt.LeftButton:
+#             self.drawing = False
+
 
 class MyApp(QMainWindow, Ui_MainWindow):
-
-    #parse_triggered = pyqtSignal()
-
     def __init__(self, parent=None):
-        super(MyApp, self).__init__(parent=parent)
+        super(MyApp, self).__init__(parent)
         self.setupUi(self)
+
         self.ogImageScene = QGraphicsScene()
         self.imageMapScene = QGraphicsScene()
+        self.ogImage.setScene(self.ogImageScene)
+        self.imageMap.setScene(self.imageMapScene)
         # self.image = QPixmap()
         # self.pixmap = QPixmap()
         # #self.editMap = imageMapping(self.pixmap, self.imageMap, self.imageMapScene, self.ogImage, self.ogImageScene)
         
-        # #self.action = Action(self.pixmap)
-        # self.viewer = photoViewer(self.ogImage, self.ogImageScene)
-        
-        # self.actionZoom_In.triggered.connect(self.handleZoomIn)
-        # self.actionOpen.triggered.connect(self.setImage)
+        #self.action = Action(self.pixmap)
         
         #self.actionUndo.triggered.connect(self.handleUndo)
         
-        # self.drawing = False
-        # self.lastPoint = QtCore.QPoint()
         
         #self.mousePressEvent(self, event)
         #self.ogImageScene.mouseMoveEvent = self.mouseMoveEvent(self, event)
@@ -44,90 +64,34 @@ class MyApp(QMainWindow, Ui_MainWindow):
         self.actionZoom_Out.triggered.connect(self.handleZoomOut)
         self.actionNormal_Size.triggered.connect(self.handleNormalSize)
         
-        #self.ogImage.mousePressEvent = drawingTool.mousePressEvent(self, event)
-        #self.ogImage.mouseMoveEvent = drawingTool.mouseMoveEvent(self, event)
-        #self.ogImage.mouseReleaseEvent = drawingTool.mouseReleaseEvent(self, event)
-        #self.ogImage.paintEvent = drawingTool.paintEvent(self, event)
+        # self.ogImage.mousePressEvent = drawingTool.mousePressEvent(self, event)
+        # self.ogImage.mouseMoveEvent = drawingTool.mouseMoveEvent(self, event)
+        # self.ogImage.mouseReleaseEvent = drawingTool.mouseReleaseEvent(self, event)
+        # self.ogImage.paintEvent = drawingTool.paintEvent(self, event)
         
         self.drawing = False
         self.lastPoint = QPoint()
-    
+
     def setImage(self):
-         
-         fileName, _ = QFileDialog.getOpenFileName(None, "select Image", "", "Image Files (*.png *.jpg *jpg *.bmp)")
-         if fileName:
-             self.image = QPixmap(fileName) ###### self.image holds the original image at all times
-             self.pixmap = self.viewer.setDefaultImage(self.image)
-             
-             
+        fileName, _  = QFileDialog.getOpenFileName(None, "select Image", "", "Image Files (*.png *.jpg *jpg *.bmp)")
+        if fileName:
+            self.image = QPixmap(fileName) ###### self.image holds the original image at all times
+            self.pixmap = self.viewer.setDefaultImage(self.image)
 
     def handleZoomIn(self):
         self.pixmap = self.viewer.zoomIn(self.pixmap)
     
     def handleZoomOut(self):
         self.pixmap = self.viewer.zoomOut(self.pixmap)
-    
+
     def handleNormalSize(self):
         self.pixmap = self.viewer.zeroZoom(self.pixmap)
-
-             
-        
-
-class photoViewer(object):
-    def __init__(self, ogImage, ogImageScene, width, height):
-        self.ogImage = ogImage
-        self.ogImageScene = ogImageScene
-        self._zoom = 0
-        self._width = width
-        self._height = height
-
-    def setDefaultImage(self, image):
-        self.image = image
-        pixmap = self.scale(self._width, self._height)
-        self.updatePixmap(pixmap)
-        return(pixmap)
-
-    def updatePixmap(self, pixmap):
-        self.ogImageScene.clear()
-        self.ogImageScene.addPixmap(pixmap)
-        self.ogImageScene.setSceneRect(QRectF(0.0, 0.0, pixmap.width(), pixmap.height()))
-
-    def scale(self, width, height):
-        if (self.image.isNull()):
-            return(QPixmap())
-        return(self.image.scaled(width, height, Qt.KeepAspectRatio))
-
-    def zoom(self, pixmap, factor):
-        pixmap = self.scale(pixmap.width()*factor, pixmap.height()*factor)
-        self.updatePixmap(pixmap)
-        return(pixmap)
-        
-    def zoomIn(self, pixmap):
-        self._zoom += 1
-        factor = 1.25
-        return(self.zoom(pixmap, factor))
-    
-    def zoomOut(self, pixmap):
-        if self._zoom == 0:
-            return(pixmap)
-        self._zoom -= 1
-        factor = 0.75
-        return(self.zoom(pixmap, factor))
-    
-    def zeroZoom(self, pixmap):
-        if self._zoom == 0:
-            return(pixmap)
-        pixmap = self.scale(self._width, self._height) #### self.pixmap is used for scaling and anything else
-        self.updatePixmap(pixmap)
-        return (pixmap)
 
 
 import resource
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    MainWindow = MyApp()
-    MainWindow.show()
+    w = MyApp()
+    w.show()
     sys.exit(app.exec_())
-
-    

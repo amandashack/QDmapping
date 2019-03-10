7775c1b33f107973d2435816ba0e1859227cb787
amanda <ajshack4@gmail.com>
2019-03-10 15:50:30 -0700

    clean back to original

--- GraphicsView.py
+++ GraphicsView.py
@@ -10,100 +10,14 @@ from GraphicsView import *
 import sys
 import time
 
-#  what does using the word "Object" do
-# can you reinstantiate self.ogImage
-
-class GraphicsScene(QtWidgets.QGraphicsScene):
-    def __init__(self, parent=None):
-        super(GraphicsScene, self).__init__(parent)#QtCore.QRectF(-500, -500, 1000, 1000), parent)
-        self._start = QtCore.QPointF()
-        self._current_rect_item = None
-
-    def mousePressEvent(self, event):
-        if self.itemAt(event.scenePos(), QtGui.QTransform()) is None:
-            self._current_rect_item = QtWidgets.QGraphicsRectItem()
-            self._current_rect_item.setBrush(QtCore.Qt.red)
-            self._current_rect_item.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable, True)
-            self.addItem(self._current_rect_item)
-            self._start = event.scenePos()
-            r = QtCore.QRectF(self._start, self._start)
-            self._current_rect_item.setRect(r)
-        super(GraphicsScene, self).mousePressEvent(event)
-
-    def mouseMoveEvent(self, event):
-        if self._current_rect_item is not None:
-            r = QtCore.QRectF(self._start, event.scenePos()).normalized()
-            self._current_rect_item.setRect(r)
-        super(GraphicsScene, self).mouseMoveEvent(event)
-
-    def mouseReleaseEvent(self, event):
-        self._current_rect_item = None
-        super(GraphicsScene, self).mouseReleaseEvent(event)
-
-class Action(QGraphicsView):
-    rectChanged = pyqtSignal(QRect)
-    
-    def __init__(self, ogImage, ogImageScene): #send in ogImage
-        QGraphicsView.__init__(self) # this makes an instance of QGraphicsView
-        self.ogImage = ogImage
-        self.ogImageScene = ogImageScene
-        self.ogImage.rubberBand = QRubberBand(QRubberBand.Rectangle, self)
-        self.setMouseTracking(True) #see what happens if this is set to False
-        self.origin = QPoint()
-        self.changeRubberBand = False
-    
-    def mousePressEvent(self, event):
-        self.ogImage.origin = event.pos()
-        self.ogImage.rubberBand = QRubberBand(QRubberBand.rectangle, self)
-        self.ogImage.setMouseTracking(True)
-        self.ogImage.origin = QPoint()
-        self.ogImage.changeRubberBand = False
-    def mouseMoveEvent(self, event):
-        if self.ogImage.changeRubberBand:
-            self.ogImage.rubberBand.setGeometry(QRect(self.origin, event.pos()).normalized())
-            self.ogImage.rectChanged.emit(self.ogImage.rubberBand.geometry())
-        QGraphicsView.mouseMoveEvent(self, event)
-    def mouseReleaseEvent(self, event):
-        self.ogImage.changeRubberBand = False
-        QGraphicsView.mouseReleaseEvent(self, event)
-    
-
-class GraphicsView(QGraphicsView):
-    rectChanged = pyqtSignal(QRect)
-
-    def __init__(self, *args, **kwargs):
-        QGraphicsView.__init__(self, *args, **kwargs)
-        self.rubberBand = QRubberBand(QRubberBand.Rectangle, self)
-        self.setMouseTracking(True)
-        self.origin = QPoint()
-        self.changeRubberBand = False
-
-    def mousePressEvent(self, event):
-        self.origin = event.pos()
-        self.rubberBand.setGeometry(QRect(self.origin, QtCore.QSize()))
-        self.rectChanged.emit(self.ogImage.rubberBand.geometry())
-        self.rubberBand.show()
-        self.changeRubberBand = True
-        QGraphicsView.mousePressEvent(self, event)
-
-    def mouseMoveEvent(self, event):
-        if self.changeRubberBand:
-            self.rubberBand.setGeometry(QRect(self.origin, event.pos()).normalized())
-            self.rectChanged.emit(self.rubberBand.geometry())
-        QGraphicsView.mouseMoveEvent(self, event)
-
-    def mouseReleaseEvent(self, event):
-        self.changeRubberBand = False
-        QGraphicsView.mouseReleaseEvent(self, event)
-
 class photoViewer(object):
-    def __init__(self, ogImage, ogImageScene, width, height):
+    def __init__(self, ogImage, ogImageScene, pixmapItem, width, height):
         self.ogImage = ogImage
         self.ogImageScene = ogImageScene
+        self.pixmapItem = pixmapItem
         self._zoom = 0
         self._width = width
         self._height = height
-
 
     def setDefaultImage(self, image):
         self.image = image
@@ -113,7 +27,9 @@ class photoViewer(object):
 
     def updatePixmap(self, pixmap):
         self.ogImageScene.clear()
-        self.ogImageScene.addPixmap(pixmap)
+        self.pixmapItem = QGraphicsPixmapItem()
+        self.pixmapItem.setPixmap(pixmap)
+        self.ogImageScene.addItem(self.pixmapItem)
         self.ogImageScene.setSceneRect(QtCore.QRectF(0.0, 0.0, pixmap.width(), pixmap.height()))
 
     def scale(self, width, height):
@@ -144,3 +60,4 @@ class photoViewer(object):
         pixmap = self.scale(self._width, self._height) #### self.pixmap is used for scaling and anything else
         self.updatePixmap(pixmap)
         return (pixmap)
+
--- MyApp.py
+++ MyApp.py
@@ -6,146 +6,25 @@ from GraphicsView import *
 import sys
 import time
 
-# class Action(object):
-#     def __init__(self):
-
-
-#     def paintEvent(self, event):
-#         painter = QtGui.QPainter(self.ogImage)
-#         painter.drawPixmap(self.ogImage.rect(), self.image)
-
-#     def mousePressEvent(self, event):
-#         if event.button() == Qt.LeftButton:
-#             self.drawing = True
-#             self.lastPoint = event.pos()
-
-#     def mouseMoveEvent(self, event):
-#         if event.buttons() and Qt.LeftButton and self.drawing:
-#             painter = QPainter(self.image)
-#             painter.setPen(QPen(Qt.red, 3, Qt.SolidLine))
-#             painter.drawLine(self.lastPoint, event.pos())
-#             self.lastPoint = event.pos()
-#             self.ogImage.update()
-
-#     def mouseReleaseEvent(self, event):
-#         if event.button == Qt.LeftButton:
-#             self.drawing = False
-
 
 class MyApp(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MyApp, self).__init__(parent)
         self.setupUi(self)
-
-        self.ogImageScene = GraphicsScene(self)
-        self.imageMapScene = GraphicsScene(self)
+        self.ogImageScene = QGraphicsScene(self)
+        self.imageMapScene = QGraphicsScene(self)
         self.ogImage.setScene(self.ogImageScene)
         self.imageMap.setScene(self.imageMapScene)
-        self.path = QPainterPath()
-        
-        # self.image = QPixmap()
-        # self.pixmap = QPixmap()
-        # #self.editMap = imageMapping(self.pixmap, self.imageMap, self.imageMapScene, self.ogImage, self.ogImageScene)
-        
-        #self.action = Action(self.pixmap)
-        
-        #self.actionUndo.triggered.connect(self.handleUndo)
-        
-        
-        #self.mousePressEvent(self, event)
-        #self.ogImageScene.mouseMoveEvent = self.mouseMoveEvent(self, event)
-        #self.ogImageScene.mouseReleaseEvent = self.mouseReleaseEvent(self, event)
-        #self.pixmap.paintEvent = self.paintEvent(self, event)    
-
-
         self.image = QPixmap()
         self.pixmap = QPixmap()
-        
-        self.viewer = photoViewer(self.ogImage, self.ogImageScene, 600, 600)
+        self.pixmapItem = QGraphicsPixmapItem()
+        self.viewer = photoViewer(self.ogImage, self.ogImageScene, self.pixmapItem, 600, 600)
         
         self.actionOpen.triggered.connect(self.setImage)
         self.actionZoom_In.triggered.connect(self.handleZoomIn)
         self.actionZoom_Out.triggered.connect(self.handleZoomOut)
         self.actionNormal_Size.triggered.connect(self.handleNormalSize)
-        
-        #self.ogImageScene.mousePressEvent = self.mousePressEvent
-        #self.ogImageScene.mouseMoveEvent = self.mouseMoveEvent
-        #self.ogImageScene.mouseReleaseEvent = self.mouseReleaseEvent
-        #self.ogImage.mouseReleaseEvent = self.mouseReleaseEvent(self, event)
-        #self.ogImage.paintEvent = self.paintEvent(self, event)
-        
-        self.drawing = False
-        self.lastPoint = QPoint()
-
-    #def mousePressEvent(self, event):
-        
-    # def paintEvent(self, event):
     
-    #     painter = QPainter()
-    #     painter.begin(self)
-    #     painter.fillRect(event.rect(), QBrush(Qt.white))
-    #     painter.setRenderHint(QPainter.Antialiasing)
-    #     painter.setPen(QPen(QBrush(Qt.red), 1, Qt.DashLine))
-    #     painter.drawRect(self.largest_rect)
-    #     painter.setPen(QPen(Qt.black))
-    #     painter.drawRect(self.clip_rect)
-    #     for i in range(4):
-    #         painter.drawRect(self.corner(i))
-        
-    #     painter.setClipRect(self.clip_rect)
-    #     painter.drawPolyline(self.polygon)
-    #     painter.setBrush(QBrush(Qt.blue))
-    #     painter.drawPath(self.path)
-    #     painter.end()
-    
-    # def corner(self, number):
-    
-    #     if number == 0:
-    #         return QRect(self.clip_rect.topLeft() - self.handle_offsets[0], QSize(8, 8))
-    #     elif number == 1:
-    #         return QRect(self.clip_rect.topRight() - self.handle_offsets[1], QSize(8, 8))
-    #     elif number == 2:
-    #         return QRect(self.clip_rect.bottomLeft() - self.handle_offsets[2], QSize(8, 8))
-    #     elif number == 3:
-    #         return QRect(self.clip_rect.bottomRight() - self.handle_offsets[3], QSize(8, 8))
-    
-    # def mousePressEvent(self, event):
-    
-    #     position = QPointF(event.pos())
-    #     print(str(position.x()) + str(position.y()))
-
-    # def mouseMoveEvent(self, event):
-    
-    #     if self.dragging is None:
-    #         return
-        
-    #     left = self.largest_rect.left()
-    #     right = self.largest_rect.right()
-    #     top = self.largest_rect.top()
-    #     bottom = self.largest_rect.bottom()
-        
-    #     point = event.pos() + self.drag_offset + self.handle_offsets[self.dragging]
-    #     point.setX(max(left, min(point.x(), right)))
-    #     point.setY(max(top, min(point.y(), bottom)))
-        
-    #     if self.dragging == 0:
-    #         self.clip_rect.setTopLeft(point)
-    #     elif self.dragging == 1:
-    #         self.clip_rect.setTopRight(point)
-    #     elif self.dragging == 2:
-    #         self.clip_rect.setBottomLeft(point)
-    #     elif self.dragging == 3:
-    #         self.clip_rect.setBottomRight(point)
-        
-    #     self.update()
-    
-    # def mouseReleaseEvent(self, event):
-    
-    #     self.dragging = None
-    
-    # def sizeHint(self):
-    #     return QSize(500, 500)
-
     def setImage(self):
         fileName, _  = QFileDialog.getOpenFileName(None, "select Image", "", "Image Files (*.png *.jpg *jpg *.bmp)")
         if fileName:
